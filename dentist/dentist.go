5056b0df28644063a56ab5099f676b1913a40ba1
Wendel Lopes <wendel.lopes777@gmail.com>
2025-12-26 20:10:07 -0300

    feat: add dentist interfaces

--- dentist/dentist.go
+++ dentist/dentist.go
@@ -1,7 +1,24 @@
 package dentist
 
 type Dentist struct {
-	Name    string
-	Address string
-	Phone   string
+	Name string
+	CRO  string
+}
+
+type Reader interface {
+	GetByCRO(cro string) (Dentist, error)
+}
+
+type Writer interface {
+	Save(dentist Dentist) error
+}
+
+type Repository struct {
+	Reader
+	Writer
+}
+
+type UseCase interface {
+	RegisterDentist(dentist Dentist) error
+	FindDentistByCRO(cro string) (Dentist, error)
 }
