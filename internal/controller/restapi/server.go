39ce172f7b7859ffff5da184fd05c68749aeeab5
Wendel Lopes <wendel.lopes777@gmail.com>
2025-12-28 19:21:55 -0300

    refactor: using other arch

--- internal/controller/restapi/server.go
+++ internal/controller/restapi/server.go
@@ -1,4 +1,4 @@
-package server
+package restapi
 
 import (
 	"fmt"
@@ -9,7 +9,7 @@ import (
 
 	_ "github.com/joho/godotenv/autoload"
 
-	"zahne/pkg/database"
+	database "zahne/pkg/postgres"
 )
 
 type Server struct {
